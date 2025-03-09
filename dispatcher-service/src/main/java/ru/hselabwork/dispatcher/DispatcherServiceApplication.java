ffc73af23373c1f79debb930cbc38f273d1021f5
denis <denisladkiy@gmail.com>
2025-03-09 22:48:45 +0300

    hide tg-token in .env

--- dispatcher-service/src/main/java/ru/hselabwork/dispatcher/DispatcherServiceApplication.java
+++ dispatcher-service/src/main/java/ru/hselabwork/dispatcher/DispatcherServiceApplication.java
@@ -1,11 +1,14 @@
 package ru.hselabwork.dispatcher;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DispatcherServiceApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("TELEGRAM_BOT_TOKEN", dotenv.get("TELEGRAM_BOT_TOKEN"));
         SpringApplication.run(DispatcherServiceApplication.class, args);
     }
 }
